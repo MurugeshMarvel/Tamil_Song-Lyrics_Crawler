34a652d78346b7868ed3ba200d1bdb60175fada3
AG <34019212+GowthamArul@users.noreply.github.com>
2023-05-02 13:41:59 +0530

    spleeter update

--- spleeter.py
+++ spleeter.py
@@ -5,43 +5,36 @@ import traceback
 
 p = os.getcwd()
 i_song = os.path.join(p, 'Sample/Songs')
-output = os.path.join(p, 'Sample\output')
+output = os.path.join(p, 'Sample/output')
 ch_dir = os.chdir('Sample/Songs')
 
 """_summary_
     Apply sleeter to remove the karaoke and get only vocal as wav
 """
 
-# for file in os.listdir(i_song):
-#     if file.endswith(".wav"):
-        # file1 = file.split(' ')[0]
-        # file_rename = f'mv {i_song}/{file} {i_song}/{file1}.mp3'
-        # os.system(file_rename)
-        # files = f"{i_song}/{file1}"
-        # spleeter separate -o /Users/murugesan.vadivel/DEV/MyRepo/In_Isai_Paadal-Extractor/output Aasai-Aasaiyai.mp3
-        # cmd = f'spleeter separate -o {output} {file}'
-        # fol = file.split('.')[0]
-        # vocal = f"{output}/{fol}"
-        # rename_cmd = f'mv {vocal}/vocals.wav {vocal}/{fol}.wav'
-        # os.system(rename_cmd)
-        # mv = f'mv {vocal}/{fol}.wav {output}'
-        # os.system(mv)
-        # rm_cmd = f'rm -rf {vocal}'
-        # os.system(rm_cmd)
-
-
 def _spleeter_convert():
     for file in tqdm.tqdm(os.listdir(i_song)):
         try:
             if file.endswith('wav'):
-                print(file)
+                print(f"Spleeter running for {file}")
                 folder = file.split(".")[0]
-                folder_path = f"{output}\{folder}"
+                folder_path = f"{output}/{folder}"
+                print("folder path is:- ", folder_path)
                 isExist = os.path.exists(folder_path)
+                print(isExist)
                 if not isExist:
                     cmd = f'spleeter separate -o {output} {file}'
                     os.system(cmd)
                     print(f"Spleeter executed for {file}")
+                else:
+                    vocal = f"{output}/{folder}"
+                    rename_cmd = f'mv {vocal}/vocals.wav {vocal}/{folder}.wav'
+                    os.system(rename_cmd)
+                    mv = f'mv {vocal}/{folder}.wav {output}'
+                    os.system(mv)
+                    rm_cmd = f'rm -rf {vocal}'
+                    os.system(rm_cmd)
+
         except Exception as exp:
             print(f"EXP - {exp}, \n {traceback.format_exc()}")
     return "spleeter excution completed for all songs"
